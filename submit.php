b0a2449f6982df819c3cb5f5005f9b0adbcdb3d0
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2024-10-14 17:41:06 +0200

    Rédaction back-end contact non-terminée

--- submit.php
+++ submit.php
@@ -1,6 +1,43 @@
 <?php
-if (($_POST["inputIdentite"] && isset($_POST["inputIdentite"]) !== null) && ($_POST["inputEmail"] && isset($_POST["inputEmail"]) !== null) && ($_POST["inputObjet"] && isset($_POST["inputObjet"]) !== null) && ($_POST["inputMessage"] && isset($_POST["inputMessage"]) !== null)) {
-    echo $_POST["inputIdentite"];
+if (
+        !empty($_POST["identite"]) &&
+        !empty($_POST["email"]) &&
+        !empty($_POST["objet"]) &&
+        !empty($_POST["message"]) &&
+        null !== $_POST["checkbox"]
+    ) {
+
+    $identite = $_POST["identite"];
+    $email = $_POST["email"];
+    $objet = $_POST["objet"];
+    $message = $_POST["message"];
+    $checkbox = $_POST["checkbox"];
+
+    // Vérification de la validité de l'identité avec REGEX contenant au moins deux mots
+    if (!preg_match("/^[a-zA-Zéèàêâùïüë\s-]{2,}$/", $identite)) {
+        echo "error-validite-identite";
+        return;
+    }
+
+    // Vérification de la validité de l'adresse e-mail
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "error-validite-email";
+        return;
+    }
+
+    $to = "edesessard@iutbayonne.univ-pau.fr";
+    $subject = "Nouvelle réponse au formulaire du portfolio";
+    $body = "Identité : " . $identite . "\nEmail : " . $email . "\nObjet : " . $objet . "\nMessage : " . $message;
+    $headers = "From: " . $email;
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "success";
+    } else {
+        echo "error-envoi-email";
+    }
+
+} else {
+    echo "error-saisi";
 }
 ?>
 
